4e4db7b858f4a58f700b5663260cfc087f645bd6
Joey <jord.vo@gmail.com>
2021-05-03 15:40:26 -0700

    Started over anew
    I added a different project that I had working with the tile editor (Tiled)

--- runme.py
+++ runme.py
@@ -1,122 +1,231 @@
-import pygame
-import time, math
-import gui
-import actor
-import controller
+"""
+Platformer Game
+"""
+import arcade
 
-##import glob
+# Constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Platformer"
 
-from constants import *
+# Constants used to scale our sprites from their original size
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+COIN_SCALING = 0.5
+SPRITE_PIXEL_SIZE = 128
+GRID_PIXEL_SIZE = (SPRITE_PIXEL_SIZE * TILE_SCALING)
 
-class Player(actor.Actor):
-    def __init__(self, *args):
-        super().__init__(*args)
-        self.left_vx = 0
-        self.right_vx = 0
-        self.vy = 0
-        self.grounded = False
-        
-    ## Create Event Handlers
-    def on_Jump(self, evt):
-        self.y-=1
-        self.vy -= JUMP_STRENGTH
-            
-    def on_Duck(self, evt):
-        pass
-    
-    def on_Move_Left(self, evt):
-        self.set_state("walk_left")
-        self.left_vx = -5
-        
-    def on_Move_Right(self, evt):
-        self.set_state("walk_right")
-        self.right_vx = +5
-        
-    def on_Stop_Duck(self, evt):
-        pass
-    
-    def on_Stop_Move_Left(self, evt):
-        self.left_vx = 0
-        
-    def on_Stop_Move_Right(self, evt):
-        self.right_vx = 0
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 6
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 18
+
+# How many pixels to keep as a minimum margin between the character
+# and the edge of the screen.
+LEFT_VIEWPORT_MARGIN = 250
+RIGHT_VIEWPORT_MARGIN = 250
+BOTTOM_VIEWPORT_MARGIN = 100
+TOP_VIEWPORT_MARGIN = 100
 
 
+class MyGame(arcade.Window):
+    """
+    Main application class.
+    """
 
-class Block(actor.Actor):
-    def __init__(self, *args):
-        super().__init__(*args)
+    def __init__(self):
 
-        
+        # Call the parent class and set up the window
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        # These are 'lists' that keep track of our sprites. Each sprite should
+        # go into a list.
+        self.coin_list = None
+        self.wall_list = None
+        self.crates_list = None
+        self.player_list = None
+
+        # Separate variable that holds the player sprite
+        self.player_sprite = None
+
+        # Our physics engine
+        self.physics_engine = None
+
+        # Used to keep track of our scrolling
+        self.view_bottom = 0
+        self.view_left = 0
+
+        # Keep track of the score
+        self.score = 0
+
+        # Load sounds
+        self.collect_coin_sound = arcade.load_sound(":resources:sounds/coin1.wav")
+        self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+    def setup(self):
+        """ Set up the game here. Call this function to restart the game. """
+
+        # Used to keep track of our scrolling
+        self.view_bottom = 0
+        self.view_left = 0
+
+        # Keep track of the score
+        self.score = 0
+
+        # Create the Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()
+
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 128
+        self.player_sprite.center_y = 128
+        self.player_list.append(self.player_sprite)
+
+        # --- Load in a map from the tiled editor ---
+
+        # Name of map file to load
+        map_name = "Basic_Hill.tmx"
+        # Name of the layer in the file that has our platforms/walls
+        platforms_layer_name = 'Platforms'
+        # Name of the layer that has items for pick-up
+        coins_layer_name = 'Coins'
+        # Name of the layer for crates specifically
+        crates_layer_name = 'Crates'
+
+        # Read in the tiled map
+        my_map = arcade.tilemap.read_tmx(map_name)
+
+        # -- Platforms
+        self.wall_list = arcade.tilemap.process_layer(map_object=my_map,
+                                                      layer_name=platforms_layer_name,
+                                                      scaling=TILE_SCALING,
+                                                      use_spatial_hash=True)
+
+        # -- Coins
+        self.coin_list = arcade.tilemap.process_layer(my_map, coins_layer_name, TILE_SCALING)
+
+        # -- Crates
+        self.crates_list = arcade.tilemap.process_layer(my_map, crates_layer_name, TILE_SCALING)
+        # --- Other stuff
+        # Set the background color
+        if my_map.background_color:
+            arcade.set_background_color(my_map.background_color)
+
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEnginePlatformer(self.player_sprite,
+                                                             self.wall_list,
+                                                             GRAVITY)
+
+    def on_draw(self):
+        """ Render the screen. """
+
+        # Clear the screen to the background color
+        arcade.start_render()
+
+        # Draw our sprites
+        self.wall_list.draw()
+        self.coin_list.draw()
+        self.crates_list.draw()
+        self.player_list.draw()
+
+        # Draw our score on the screen, scrolling it with the viewport
+        score_text = f"Score: {self.score}"
+        arcade.draw_text(score_text, 10 + self.view_left, 10 + self.view_bottom,
+                         arcade.csscolor.WHITE, 18)
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+                arcade.play_sound(self.jump_sound)
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
+
+        # See if we hit any coins
+        coin_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
+                                                             self.coin_list)
+
+        # Loop through each coin we hit (if any) and remove it
+        for coin in coin_hit_list:
+            # Remove the coin
+            coin.remove_from_sprite_lists()
+            # Play a sound
+            arcade.play_sound(self.collect_coin_sound)
+            # Add one to the score
+            self.score += 1
+
+        # --- Manage Scrolling ---
+
+        # Track if we need to change the viewport
+
+        changed = False
+
+        # Scroll left
+        left_boundary = self.view_left + LEFT_VIEWPORT_MARGIN
+        if self.player_sprite.left < left_boundary:
+            self.view_left -= left_boundary - self.player_sprite.left
+            changed = True
+
+        # Scroll right
+        right_boundary = self.view_left + SCREEN_WIDTH - RIGHT_VIEWPORT_MARGIN
+        if self.player_sprite.right > right_boundary:
+            self.view_left += self.player_sprite.right - right_boundary
+            changed = True
+
+        # Scroll up
+        top_boundary = self.view_bottom + SCREEN_HEIGHT - TOP_VIEWPORT_MARGIN
+        if self.player_sprite.top > top_boundary:
+            self.view_bottom += self.player_sprite.top - top_boundary
+            changed = True
+
+        # Scroll down
+        bottom_boundary = self.view_bottom + BOTTOM_VIEWPORT_MARGIN
+        if self.player_sprite.bottom < bottom_boundary:
+            self.view_bottom -= bottom_boundary - self.player_sprite.bottom
+            changed = True
+
+        if changed:
+            # Only scroll to integers. Otherwise we end up with pixels that
+            # don't line up on the screen
+            self.view_bottom = int(self.view_bottom)
+            self.view_left = int(self.view_left)
+
+            # Do the scrolling
+            arcade.set_viewport(self.view_left,
+                                SCREEN_WIDTH + self.view_left,
+                                self.view_bottom,
+                                SCREEN_HEIGHT + self.view_bottom)
+
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode(SCREEN_SIZE)
-    
-    # set up the main controller
-    em = gui.EventManager()
-    ctrl = controller.Controller(em, screen)
-    
-    # create the "megaman"
-    player = Player(0,300)
-    player.load_animation("walk_right","Images\\Sprites\\run*.png", scaling = 0.1)
-    player.load_animation("walk_left","Images\\Sprites\\run*.png", scaling = 0.1, reverse=True )
-    player.set_state("walk_right")
-    
-    ctrl.add_actor(player)
-    
-    #create the blocks
-    for x in range(6):
-        block = Block(x*64,416)
-        block.load_animation("static", "Images\\Basic_Block.png");
-        block.set_state("static")
-        ctrl.add_block(block)
-    for x in range(6):
-        block = Block(x*64+192, 480)
-        block.load_animation("static", "Images\\Basic_Block.png");
-        block.set_state("static")
-        ctrl.add_block(block)
+    """ Main method """
+    window = MyGame()
+    window.setup()
+    arcade.run()
 
-    # kickstart the game
-    em.run()
 
-def collide_test():
-    pygame.init()
-    screen = pygame.display.set_mode(SCREEN_SIZE)
-    
-    # set up the main controller
-    em = gui.EventManager()
-    ctrl = controller.Controller(em, screen)
-    
-    # create the "megaman"
-    player = Player(0,300)
-    player.load_animation("walk_right","Images\\Sprites\\run*.png", scaling = 0.1)
-    player.load_animation("walk_left","Images\\Sprites\\run*.png", scaling = 0.1, reverse=True )
-    player.set_state("walk_right")
-    
-    ctrl.add_actor(player)
-    
-    #create the blocks
-    for x in range(6):
-        block = Block(x*64,416)
-        block.load_animation("static", "Images\\Basic_Block.png");
-        block.set_state("static")
-        ctrl.add_block(block)
-    for x in range(6):
-        block = Block(x*64+192, 480)
-        block.load_animation("static", "Images\\Basic_Block.png");
-        block.set_state("static")
-        ctrl.add_block(block)
-    block = Block(240, 400)
-
-    # kickstart the game
-    em.run()
-
-        
 if __name__ == "__main__":
-    collide_test()
-
-##Make a viewport for controlling the screen and world chords together
-    
-##    viewport = Viewport(0,0,SCREEN_WIDTH,SCREEN_HEIGHT)
-##    controller.viewport = viewport
+    main()
